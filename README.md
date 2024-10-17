0f2e75a6b2400609afebfa95f9dfbf67725f8c6f
nazarpot <nazarpot@gmail.com>
2024-10-17 13:13:08 -0700

    added clipboard functionality

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # 2llamas
 
 ## What are the expand files?
+Take input:
+* p = Pick Up
+* d = Drop Off
+* w = Weight
+* r = Rate
+* t = Temperature
+
+expand takes such params (any number) and converts to a more readable
+format. It also automatically adds to clipboard, so you can past it 
+wherever needed.
+
+##### OS specifics
 expand is for mac
 expand.exe is for windows
 expand.go is the source code
@@ -8,8 +20,17 @@ expand.go is the source code
 ### On Mac
 ./expand [params]
 
+example:
+```
+./expand p1200 d1200 w2000 r0.50 t32
+```
 ### On Windows
 expand.exe [params]
+
+example:
+```
+expand.exe p1200 d1200 w2000 r0.50 t32
+```
 
 ## Before running a .sh file for the first time
 You will need to add persmission to execute the files. execute the
--- expand.go
+++ expand.go
@@ -3,31 +3,55 @@ package main
 import (
 	"fmt"
 	"os"
+	"golang.design/x/clipboard"
 )
 
-func processer(input string) {
+func copy(output string) {
+	err := clipboard.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	clipboard.Write(clipboard.FmtText, []byte(output))
+}
+
+func processer(input string) string {
+	output := ""
 	var v = input[0]
 	switch v {
 	case 'p':
-		fmt.Println("Pick up: ", input[1:])
+		var stmt = "Pick up: " + input[1:]
+		fmt.Println(stmt)
+		output = output + stmt + "\n"
 	case 'd':
-		fmt.Println("Drop off: ", input[1:])
+		var stmt = "Drop off: " + input[1:]
+		fmt.Println(stmt)
+		output = output + stmt + "\n"
 	case 'w':
-		fmt.Println("Weight: ", input[1:])
+		stmt := "Weight: " + input[1:]
+		fmt.Println(stmt)
+		output = output + stmt + "\n"
 	case 'r':
-		fmt.Println("Rate: ", input[1:])
+		stmt := "Rate: " + input[1:]
+		fmt.Println(stmt)
+		output = output + stmt + "\n"
 	case 't':
-		fmt.Println("Temperature:", input[1:])
+		stmt := "Temperature: " + input[1:]
+		fmt.Println(stmt)
+		output = output + stmt + "\n"
 	}
-
+	return output
 }
 
 func reciever() {
 	args := os.Args[1:]
 
+	output := ""
 	for i := 0; i < len(args); i++ {
-		processer(args[i])
+		line := processer(args[i])
+		output += line
 	}
+	copy(output)
 }
 func main() {
 	reciever()
